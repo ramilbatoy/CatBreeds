634dc43feb1261031fb61b37605b20912df5efec
Ramil.Batoy <ramil.batoy@gmail.com>
2019-10-04 14:34:13 +0800

    Update for Single Cat Page(loads info of the specific cat breed based on the view info button) - remove unwanted code Update App.css and App.js (main related update)

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-This project was React-bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was React-bootstrapped with [Create React App]
+
+- React app that loads images of Cats
+
+detail :
+
+-- 2 Pages: HomePage and Single Cat Page 
 
 
+Home Page:
+    -> Select Input Box : Loads Breeds of Cats Via API :  https://docs.thecatapi.com/
+
+    -> Fetch images After selecting a breed
+
+    -> View Detail button on each email that will load the single cat page
+
+ Single Cat Page:
+
+ 	-> Load detail of Specific Cat breed based on the view detail button 
--- src/component/App.js
+++ src/component/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import '../css/App.css';
+import { FaPlus } from 'react-icons/fa';
+import { Button } from 'react-bootstrap';
 
 axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
-axios.defaults.headers.common['x-api-key'] = 'DEMO-API-KEY';
+axios.defaults.headers.common['x-api-key'] = '0efff5a7-fc58-44f0-b9aa-c48640838f85';
 
 class App extends Component {
 
@@ -11,37 +13,80 @@ class App extends Component {
       const res = await axios('/breeds');
       return res.data;
   }
-  async getCatsImagesByBreed(breed_id, amount) {
-      const res = await axios('/images/search?breed_ids='+breed_id + "&limit="+ amount);
-      
+  async getCatsImagesByBreed(breed_id, page) {
+      const res = await axios('/images/search?breed_ids='+breed_id + "&order=asc&page=" + (page-1) + "&limit="+ 6 );
+      // check on console if value 
       console.table(res.data)
       return res.data;
   }
 
-  async loadBreedImages() {
-    console.log('Load Breed Images:', this.state.selected_breed)
+  async loadBreedImages(page) {
+    console.log('Load Breed Images:', this.state.selected_breed);
+    //const page = this.state.page;
 
-    let breed_images = await this.getCatsImagesByBreed(this.state.selected_breed, 6)
-
-    this.setState({ images: breed_images });
+    let breed_images = await this.getCatsImagesByBreed(this.state.selected_breed, page)
+    let next_images = await this.getCatsImagesByBreed(this.state.selected_breed, page+1)
+    const images = this.state.images.concat(breed_images);
+    console.log("[Load Images]",images);
+    this.setState({ images: images, nextImages: next_images });
   }
+
+  nextpage = (page) => {
+      //load new set of images 
+    this.setState({ page: page});
+    this.loadBreedImages(page);
+  };
+
 
   constructor(...args) {
 
       super(...args);
       this.state = {
         images: [],
+        page: 1,
+        nextImages: [],
         breeds: [], 
+        catpage: [],
+        hide: false,
         selected_breed: 0
       };
 
     this.onBreedSelectChange = this.onBreedSelectChange.bind(this);
+    this.catpagedetail = this.catpagedetail.bind(this);
+    this.toggle = this.toggle.bind(this);
   }
   async onBreedSelectChange(e) {
     console.log("Breed Selected. ID:",e.target.value)
-    await this.setState({selected_breed:e.target.value});
-    await this.loadBreedImages();
+    await this.setState({
+        selected_breed:e.target.value,
+        images:[],
+        catpage: [],
+        page:1
+    });
+    await this.loadBreedImages(1);
   }
+  toggle() {
+    this.setState({hide: !this.state.hide});
+  }
+ catpagedetail(url,origin, name, temperament, description) {
+    //load catpage details
+      let tempcatpage = this.state.catpage;
+      let sampledetails = [
+        {
+          'url' : url,
+          'name' : name,
+          'origin' : origin,
+          'temperment' : temperament,
+          'description' : description
+        }];
+      tempcatpage = sampledetails;
+      this.toggle();
+      this.setState({ 
+        catpage: sampledetails
+      });
+
+  };
+
   componentDidMount() {
       if (this.state.breeds.length===0) {
           (async () => {
@@ -57,17 +102,53 @@ class App extends Component {
   render() {
       return (
         <div className="container">
-            <div className="listbreed">
-                <select value={this.state.selected_breed} 
-                        onChange={this.onBreedSelectChange}>
-                    {this.state.breeds.map((breed) => <option key={breed.id} value={breed.id}>{breed.name}</option>)}
-                </select>
-            </div>
+          
             <div className="row">                 
-                <div className="mycatlist">
-                {this.state.images.map((image) => <div className="col-xs-12 col-sm-4"><img className="cat-image" alt="" src={image.url}></img></div>)}
+                <div className= {
+                'mycatlist ' +
+                (this.state.hide ? 'hide' : '')
+            }>
+                  <div className="listbreed col-xs-12 col-sm-12">
+                    <select value={this.state.selected_breed} 
+                            onChange={this.onBreedSelectChange}>
+                            <option key={'00'} value={'select'}>Select Bread</option>  
+                        {this.state.breeds.map((breed) => <option key={breed.id} value={breed.id}>{breed.name}</option>)}
+                    </select>
+                  </div>
+                  <div class="listcontainer aligncenter">
+                      {this.state.images.map((image, index) => <div className="col-xs-12 col-sm-4" key={index}><img className="cat-image" alt="" src={image.url}></img>               
+                      <div><Button variant="secondary" size="sm" block className="button" onClick={ () => 
+                        this.catpagedetail(image.url, image.breeds[0].origin,image.breeds[0].name, image.breeds[0].temperament, image.breeds[0].description)}>                   
+                        View details </Button> </div>
+                      </div>)}
+                      {this.state.nextImages.length > 0 && (<Button variant="dark" size="lg" className="button centerfix" onClick={() => this.nextpage(this.state.page + 1)}> < FaPlus /> Load More... </Button>)}
+                  </div>
                 </div>
-            </div>   
+                <div className= {
+                    'mycatpage ' +
+                    (this.state.hide ? '' : 'hide')
+                  }>
+                   {this.state.catpage.map((catpage) => 
+                   <div className="col-xs-12 col-sm-12" >
+                     <div><Button variant="success"  size="lg" className="button bback" onClick={this.toggle}> Back </Button> </div>
+                     <img className="cat-image" alt="" src={catpage.url}></img>
+                      <h2>{catpage.name} </h2>
+                      <div className="infocatpage">
+                        <label className="col-md-2 text-md-right" htmlFor="petName" readOnly>Origin: </label>
+                        <div className="col-md-10">{catpage.origin} </div> 
+                      </div>
+                      <div className="infocatpage">
+                        <label className="col-md-2  text-md-right" htmlFor="petName" readOnly>temperment: </label>
+                        <div className="col-md-10">{catpage.temperment} </div> 
+                      </div>
+                      <div className="infocatpage">
+                        <label className="col-md-2  text-md-right" htmlFor="petName" readOnly>Description: </label>
+                        <div className="col-md-10">{catpage.description} </div>    
+                      </div>                                    
+                  </div>)}
+                </div>
+            </div>
+            
         </div>
       );
   }
